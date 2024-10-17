6c07c1ed577ffb5c05dafb86d621507805828080
kpq854 <crkeo169@gmail.com>
2024-10-17 22:11:24 +0700

    basic sign in and sign up UI

--- MedSy/MedSy/App.xaml.cs
+++ MedSy/MedSy/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml;
+﻿using MedSy.Views;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -15,7 +16,6 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -41,10 +41,10 @@ namespace MedSy
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
+            m_window = new SignInWindow();
             m_window.Activate();
         }
 
-        private Window m_window;
+        private SignInWindow m_window;
     }
 }
