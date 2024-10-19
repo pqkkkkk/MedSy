5c70273e0c285f3dc62d0e78454c3d20b57ee7d3
ablacker2311 <qthong2004@gmail.com>
2024-10-20 00:30:47 +0700

    add Doctor Dashboard

--- MedSy/MedSy/Views/DoctorDashboard.xaml.cs
+++ MedSy/MedSy/Views/DoctorDashboard.xaml.cs
@@ -1,4 +1,8 @@
-using MedSy.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -6,29 +10,22 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace MedSy
+namespace MedSy.Views
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class DoctorDashboard : Page
     {
-        public MainWindow()
+        public DoctorDashboard()
         {
             this.InitializeComponent();
-            this.Title = "Doctor page";
-            content.Navigate(typeof(DoctorDashboard));
         }
     }
 }
